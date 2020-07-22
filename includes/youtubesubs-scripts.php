2df4e84d2b00c96d66e6aae7a768d1a6a6101fbe
dstearle <dallas.st.earle@gmail.com>
2020-07-22 15:21:20 -0700

    Set up youtubesubs-scripts.php

--- includes/youtubesubs-scripts.php
+++ includes/youtubesubs-scripts.php
@@ -1,6 +1,14 @@
 <?php 
 
     // Add Scripts
-    function yts_add_scripts(){}
-    
+    function yts_add_scripts() {
+
+        // Add Main Css
+        wp_enqueue_style('yts-main-style', plugins_url() . '/youtubesubs/css/style.css');
+
+        // Add Main JS
+        wp_enqueue_script('yts-main-script', plugins_url() . '/youtubesubs/js/main.js');
+        
+    }
+
 ?>
