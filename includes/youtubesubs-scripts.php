7b342539ff73c4ddb4f75459e6745babd6dedab7
dstearle <dallas.st.earle@gmail.com>
2020-07-22 14:32:42 -0700

    First commit

---
+++ includes/youtubesubs-scripts.php
@@ -0,0 +1,6 @@
+<?php 
+
+    // Add Scripts
+    function yts_add_scripts(){}
+    
+?>
---
+++ youtubesubs.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Youtube Subs
+Plugin URI: https://dstearle.com
+Description: Display Youtube sub button and count
+Version: 0.1.0
+Author: Dallas Earle
+Author URI: https://dstearle.com
+*/
+
+// Exit if accessed directly
+if(!defined('ABSPATH')){
+    exit;
+}
